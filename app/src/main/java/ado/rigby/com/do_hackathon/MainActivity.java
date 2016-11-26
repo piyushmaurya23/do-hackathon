e888a7a561336c9dadbc3a92054539573b713e8c
Soumya <singh.94soumya@gmail.com>
2016-11-26 13:13:23 +0530

    first commit activity

--- app/src/main/java/ado/rigby/com/do_hackathon/MainActivity.java
+++ app/src/main/java/ado/rigby/com/do_hackathon/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText token;
+    Button token_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +21,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        token=(EditText)findViewById(R.id.token);
+        token_btn=(Button)findViewById(R.id.token_btn);
+        token_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+
+    }
     }
 
     @Override
