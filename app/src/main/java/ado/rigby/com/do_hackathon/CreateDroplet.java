2bc1f0ce4157ecf1ac5ecfe09ac7c86a74c8477d
roopeshkewlani <roopesh.kewlani90@gmail.com>
2016-11-26 14:39:38 +0530

    new activity

---
+++ app/src/main/java/ado/rigby/com/do_hackathon/CreateDroplet.java
@@ -0,0 +1,13 @@
+package ado.rigby.com.do_hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateDroplet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_droplet);
+    }
+}
